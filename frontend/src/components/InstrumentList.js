bd961113bd91e004117b243b0566bcb2f188f30e
luigicarlini <luigicarlini@libero.it>
2023-09-29 12:00:49 +0200

    instrument List with the Status shown corrected

--- frontend/src/components/InstrumentList.js
+++ frontend/src/components/InstrumentList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getAllInstruments, bookInstrument, getInstrumentStatus } from '../api/api';
-import { useUserContext } from '../context/UserContext';  // <-- Note the corrected import
+import { useUserContext } from '../context/UserContext';
 
 const InstrumentList = () => {
     const [instruments, setInstruments] = useState([]);
@@ -11,6 +11,19 @@ const InstrumentList = () => {
         const fetchData = async () => {
             const allInstruments = await getAllInstruments();
             setInstruments(allInstruments);
+             // Fetch statuses for each instrument
+             const statuses = await Promise.all(
+                allInstruments.map(instrument => getInstrumentStatus(instrument._id))
+            );
+
+            // Map the statuses to their corresponding instrument ID
+            const statusMap = {};
+            allInstruments.forEach((instrument, index) => {
+                statusMap[instrument._id] = statuses[index].availability ? "Available" : "Booked";
+            });
+
+            // Set the instrumentStatuses state with the fetched statuses
+            setInstrumentStatuses(statusMap);
         };
         fetchData();
     }, []);
@@ -19,30 +32,32 @@ const InstrumentList = () => {
         if (user) {
             const bookedInstrument = await bookInstrument(id, user._id);
             // Assuming the API returns a new status for the instrument after booking
-            setInstrumentStatuses(prevStatuses => ({ ...prevStatuses, [id]: bookedInstrument.status }));
+            setInstrumentStatuses(prevStatuses => ({ ...prevStatuses, [id]: bookedInstrument.availability ? "Available" : "Booked" }));
         } else {
             console.log("You must be logged in to book an instrument.");
         }
     };
 
-    const handleGetStatus = async (id) => {
-        const status = await getInstrumentStatus(id);
-        setInstrumentStatuses(prevStatuses => ({ ...prevStatuses, [id]: status }));
-    };
+    // Future Use if we want to add a button to check the Status
+    // const handleGetStatus = async (id) => {
+    //     const status = await getInstrumentStatus(id);
+    //     setInstrumentStatuses(prevStatuses => ({ ...prevStatuses, [id]: status.availability ? "Available" : "Booked" }));
+    //     // setInstrumentStatuses(prevStatuses => ({ ...prevStatuses, [id]: status }));
+    // };
 
     return (
         <div>
             {instruments.map((instrument) => (
-                <div key={instrument._id}>
-                    <span>{instrument.name}</span>
+                <div key={instrument._id} style={{ marginBottom: '10px' }}>
+                    <span>{instrument.instrumentName}</span>
                     <span> - Status: {instrumentStatuses[instrument._id] || "Unknown"}</span>
                     <button 
                         disabled={instrumentStatuses[instrument._id] === 'Booked'}
                         onClick={() => handleBookInstrument(instrument._id)}
+                        style={{ margin: '0 10px 0 5px' }}
                     >
                         Book
                     </button>
-                    <button onClick={() => handleGetStatus(instrument._id)}>Check Status</button>
                 </div>
             ))}
         </div>
@@ -50,4 +65,3 @@ const InstrumentList = () => {
 };
 
 export default InstrumentList;
-
