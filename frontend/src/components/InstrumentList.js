8ac9b30e312b65040de8149bb62061859eb946bf
luigicarlini <luigicarlini@libero.it>
2023-09-29 14:27:53 +0200

    Rendering InstrumentList (GitHub style)

--- frontend/src/components/InstrumentList.js
+++ frontend/src/components/InstrumentList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getAllInstruments, bookInstrument, getInstrumentStatus } from '../api/api';
 import { useUserContext } from '../context/UserContext';
+import './InstrumentList.css'; // Import the styles
 
 const InstrumentList = () => {
     const [instruments, setInstruments] = useState([]);
@@ -48,13 +49,20 @@ const InstrumentList = () => {
     return (
         <div>
             {instruments.map((instrument) => (
-                <div key={instrument._id} style={{ marginBottom: '10px' }}>
-                    <span>{instrument.instrumentName}</span>
-                    <span> - Status: {instrumentStatuses[instrument._id] || "Unknown"}</span>
+                <div key={instrument._id} style={{ marginBottom: '10px', border: '1px solid #014C8C', padding: '15px', borderRadius: '5px' }}>
+                    <div style={{ fontWeight: 'bold', fontSize: '1.1em', color: '#014C8C' }}>{instrument.instrumentName}</div>
+                    <div><span style={{ fontWeight: 'bold' }}>Status:</span> {instrumentStatuses[instrument._id] || "Unknown"}</div>
+                    <div><span style={{ fontWeight: 'bold' }}>Manufacturer:</span> {instrument.manufacturer}</div>
+                    <div><span style={{ fontWeight: 'bold' }}>Model:</span> {instrument.model}</div>
+                    <div><span style={{ fontWeight: 'bold' }}>Frequency Range:</span> {instrument.frequencyRange}</div>
+                    <div><span style={{ fontWeight: 'bold' }}>Description:</span> {instrument.description}</div>
+                    <div><span style={{ fontWeight: 'bold' }}>Booked by:</span> {instrument.bookedBy || "N/A"}</div>
+                    <div><span style={{ fontWeight: 'bold' }}>Booked from:</span> {instrument.bookedFrom || "N/A"}</div>
+                    <div><span style={{ fontWeight: 'bold' }}>Booked until:</span> {instrument.bookedUntil || "N/A"}</div>
                     <button 
                         disabled={instrumentStatuses[instrument._id] === 'Booked'}
                         onClick={() => handleBookInstrument(instrument._id)}
-                        style={{ margin: '0 10px 0 5px' }}
+                        style={{ marginTop: '10px', backgroundColor: '#28a745', color: 'white', border: 'none', padding: '5px 10px', borderRadius: '4px', cursor: 'pointer' }}
                     >
                         Book
                     </button>
@@ -65,3 +73,6 @@ const InstrumentList = () => {
 };
 
 export default InstrumentList;
+
+
+
